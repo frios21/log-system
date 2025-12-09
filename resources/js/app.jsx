37f3eb6bc550df088480fab1a18fd08a6ce5ca48
skarwinter <feiipe.rioz123@gmail.com>
2025-12-09 14:56:54 -0300

    changed files

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -5,6 +5,6 @@ import "../css/ui-modern.css";
 
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
 const root = createRoot(document.getElementById("app"));
 root.render(<App />);
