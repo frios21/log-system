8db910d1d004113616d124c4f86ac3960f3fcdbb
skarwinter <feiipe.rioz123@gmail.com>
2025-12-11 15:27:01 -0300

    changed files

--- resources/js/components/sidebar/CargasList.jsx
+++ resources/js/components/sidebar/CargasList.jsx
@@ -595,7 +595,6 @@ export default function CargasList() {
                     onClose={() => setContactModalTarget(null)}
                     onSelect={async (contact) => {
                         await updateManualCarga(contactModalTarget.id, {
-                            vendor_id: contact.id,
                             vendor_name: contact.display_name || contact.name,
                         });
                         setContactModalTarget(null);
